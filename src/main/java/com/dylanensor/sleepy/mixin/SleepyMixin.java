7a659947d3a289504889898c0ed2be3b355c9fb5
dylan <dylan@dylanensor.com>
2021-12-17 17:46:27 -0600

    changed logger output

--- src/main/java/com/dylanensor/sleepy/mixin/SleepyMixin.java
+++ src/main/java/com/dylanensor/sleepy/mixin/SleepyMixin.java
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class SleepyMixin {
     @Inject(at = @At("HEAD"), method = "init()V")
     private void init(CallbackInfo info) {
-        SleepyAdventures.LOGGER.info("This line is printed by an example mod mixin!");
+        SleepyAdventures.LOGGER.info("mixins napping...");
     }
 }
