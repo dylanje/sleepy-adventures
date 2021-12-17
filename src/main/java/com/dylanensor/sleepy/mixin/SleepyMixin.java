00757c25fd2eb31173eebea3f03b93437109d044
dylan <dylan@dylanensor.com>
2021-12-17 11:44:59 -0600

    updating some defaults

---
+++ src/main/java/com/dylanensor/sleepy/mixin/SleepyMixin.java
@@ -0,0 +1,16 @@
+package com.dylanensor.sleepy.mixin;
+
+import com.dylanensor.sleepy.SleepyAdventures;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class SleepyMixin {
+    @Inject(at = @At("HEAD"), method = "init()V")
+    private void init(CallbackInfo info) {
+        SleepyAdventures.LOGGER.info("This line is printed by an example mod mixin!");
+    }
+}
